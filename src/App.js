c0d59a666c254427cb1e1ca72e775c3a72a5b02a
TheChunkyPumpkinhead <chriscampos@remainobscure.com>
2024-03-18 15:59:17 -0700

    created a variable to know what step i am on.

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ const messages = [
 ];
 
 export default function App() {
+  const step = 1;
+
   return (
     <div className="steps">
       <div className="numbers">
@@ -12,15 +14,17 @@ export default function App() {
         <div>2</div>
         <div>3</div>
       </div>
+
       {/* {/message */}
-      <p className="message">Hello</p>
+      <p className="message">
+        Step {step}: {messages[step - 1]}
+      </p>
+
       <div className="buttons">
         <button style={{ backgroundColor: "blue", color: "#fff" }}>
           Prevous
         </button>
-        <button style={{ backgroundColor: "blue", color: "#fff" }}>
-          Prevous
-        </button>
+        <button style={{ backgroundColor: "blue", color: "#fff" }}>Next</button>
       </div>
     </div>
   );
