0d97a953fd22e3f8300eebb2e035f740675ab437
TheChunkyPumpkinhead <chriscampos@remainobscure.com>
2024-03-18 15:38:34 -0700

    added an outside array to pull messages from.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+
 export default function App() {
   return (
     <div className="steps">
       <div className="numbers">
-        <div>1</div>
+        <div className="active">1</div>
         <div>2</div>
         <div>3</div>
       </div>
-      {/* {/message */} */}
+      {/* {/message */}
       <p className="message">Hello</p>
       <div className="buttons">
         <button style={{ backgroundColor: "blue", color: "#fff" }}>
