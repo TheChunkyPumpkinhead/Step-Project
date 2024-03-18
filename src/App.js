9d9fea97c7990a1330d7f1e90e1d874ae0a419a7
TheChunkyPumpkinhead <chriscampos@remainobscure.com>
2024-03-18 15:35:02 -0700

    added buttons and a message

--- src/App.js
+++ src/App.js
@@ -1,3 +1,21 @@
 export default function App() {
-  return <div>Hello WOrld</div>;
+  return (
+    <div className="steps">
+      <div className="numbers">
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+      </div>
+      {/* {/message */} */}
+      <p className="message">Hello</p>
+      <div className="buttons">
+        <button style={{ backgroundColor: "blue", color: "#fff" }}>
+          Prevous
+        </button>
+        <button style={{ backgroundColor: "blue", color: "#fff" }}>
+          Prevous
+        </button>
+      </div>
+    </div>
+  );
 }
