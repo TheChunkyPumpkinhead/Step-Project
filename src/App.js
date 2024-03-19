6b43b403d079d51d291ff179178e3086dac7f223
TheChunkyPumpkinhead <chriscampos@remainobscure.com>
2024-03-19 15:42:53 -0700

    created a div element

--- src/App.js
+++ src/App.js
@@ -25,30 +25,32 @@ export default function App() {
   }
 
   return (
-    <div className="steps">
-      <div className="numbers">
-        <div className={step >= 1 ? "active" : ""}>1</div>
-        <div className={step >= 2 ? "active" : ""}>2</div>
-        <div className={step >= 3 ? "active" : ""}>3</div>
-      </div>
+    <div>
+      <div className="steps">
+        <div className="numbers">
+          <div className={step >= 1 ? "active" : ""}>1</div>
+          <div className={step >= 2 ? "active" : ""}>2</div>
+          <div className={step >= 3 ? "active" : ""}>3</div>
+        </div>
 
-      <p className="message">
-        Step {step}: {messages[step - 1]}
-      </p>
+        <p className="message">
+          Step {step}: {messages[step - 1]}
+        </p>
 
-      <div className="buttons">
-        <button
-          style={{ backgroundColor: "blue", color: "#fff" }}
-          onClick={handlePrevious}
-        >
-          Previous
-        </button>
-        <button
-          style={{ backgroundColor: "blue", color: "#fff" }}
-          onClick={handleNext}
-        >
-          Next
-        </button>
+        <div className="buttons">
+          <button
+            style={{ backgroundColor: "blue", color: "#fff" }}
+            onClick={handlePrevious}
+          >
+            Previous
+          </button>
+          <button
+            style={{ backgroundColor: "blue", color: "#fff" }}
+            onClick={handleNext}
+          >
+            Next
+          </button>
+        </div>
       </div>
     </div>
   );
